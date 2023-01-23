d8b7043bbb5fc808957da18c31ad566cfe965324
Archie T. Saquilabon <32692216+ASaquilabon@users.noreply.github.com>
2023-01-23 15:37:21 -0800

    Updates

---
+++ precache-manifest.d0f15c171650b0b1afaeea7b7251fb32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc2abbfac97e629ac6e9",
+    "url": "/React-Todo-App/static/css/main.f3d109af.chunk.css"
+  },
+  {
+    "revision": "cc2abbfac97e629ac6e9",
+    "url": "/React-Todo-App/static/js/main.cc2abbfa.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/React-Todo-App/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "90f8273e3ea2e9a9580f",
+    "url": "/React-Todo-App/static/css/2.8d706d98.chunk.css"
+  },
+  {
+    "revision": "90f8273e3ea2e9a9580f",
+    "url": "/React-Todo-App/static/js/2.90f8273e.chunk.js"
+  },
+  {
+    "revision": "c543b6ecd417d29f4bb7",
+    "url": "/React-Todo-App/static/js/runtime~main.c543b6ec.js"
+  },
+  {
+    "revision": "8cdb3f3db35105d5f4851bbb02122c84",
+    "url": "/React-Todo-App/index.html"
+  }
+];
